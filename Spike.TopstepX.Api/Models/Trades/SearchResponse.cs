d1f9f0427adc121aa21be82074ea5cc688e41bad
Adam <adam.marquette@gmail.com>
2025-06-07 11:48:13 -0500

    Fix

--- Spike.TopstepX.Api/Models/Trades/SearchResponse.cs
+++ Spike.TopstepX.Api/Models/Trades/SearchResponse.cs
@@ -1,13 +1,16 @@
 ﻿namespace Spike.TopstepX.Api.Models.Trades
 {
     /// <summary>
-    /// Represents a response containing a list of trades for a specific account.
+    /// Represents a response containing a list of trades for a 
+    /// specific account.
     /// </summary>
-    /// <param name="trades">The <see cref="List{Trade}" /> of matching trades.</param>
+    /// <param name="trades">
+    /// The <see cref="List{Trade}" /> of matching trades.
+    /// </param>
     public record SearchResponse(List<Trade> trades = default!) : DefaultResponse
     {
         /// <summary>  
-        ///     Gets or sets a <see cref="List{Trade}" /> of matching trades.
+        ///     Gets a <see cref="List{Trade}" /> of matching trades.
         /// </summary>  
         public required List<Trade> Trades { get; set; } = trades;
     }
